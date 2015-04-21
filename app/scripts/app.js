0a1ac758c27ad702f60b8d0b3214ebaa12522820
Eduardo Garcia <egarcia@ooyala.com>
2015-04-20 23:43:53 -0500

    Add basic project structure

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name standardModelApp
+ * @description
+ * # standardModelApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('standardModelApp', []);
